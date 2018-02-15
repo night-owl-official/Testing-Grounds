8f1e3c82f365f0085ae723d280c21381360f2bc1
Daniel Grieco <danny26.business@gmail.com>
2018-02-15 17:51:28 +0100

    - Done some refactoring of the content browser

--- Source/TestingGrounds/TestingGroundsHUD.cpp
+++ Source/TestingGrounds/TestingGroundsHUD.cpp
@@ -10,7 +10,7 @@
 ATestingGroundsHUD::ATestingGroundsHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Weapons/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
